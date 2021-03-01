3b43f607126708bb49520773a57a93483756a9fc
Eli Richey <eli@elirichey.com>
2021-02-28 20:57:30 -0500

    working on permissions, not working yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,130 @@
 import React, {Component} from 'react';
-import {StyleSheet, StatusBar, View, Text} from 'react-native';
+import {
+  StyleSheet,
+  StatusBar,
+  Platform,
+  PermissionsAndroid,
+  View,
+  ToastAndroid,
+  Linking,
+  Alert,
+  Text,
+} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      permissions: false,
+      location: null,
+    };
+  }
+
+  componentDidMount() {
+    if (this.hasLocationPermission) {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position);
+        },
+        (error) => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    }
+  }
+
+  /************************************ PERMISSIONS ************************************/
+
+  hasLocationPermissionIOS = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+
+    const status = await Geolocation.requestAuthorization('whenInUse');
+
+    if (status === 'granted') {
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        `Turn on Location Services to allow "App" to determine your location.`,
+        '',
+        [
+          {text: 'Go to Settings', onPress: openSetting},
+          {text: "Don't Use Location", onPress: () => {}},
+        ],
+      );
+    }
+
+    return false;
+  };
+
+  hasLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      const hasPermission = await this.hasLocationPermissionIOS();
+
+      this.setState({permissions: true});
+
+      return hasPermission;
+    }
+
+    if (Platform.OS === 'android' && Platform.Version < 23) {
+      return true;
+    }
+
+    const hasPermission = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (status === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    }
+
+    if (status === PermissionsAndroid.RESULTS.DENIED) {
+      ToastAndroid.show(
+        'Location permission denied by user.',
+        ToastAndroid.LONG,
+      );
+    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      ToastAndroid.show(
+        'Location permission revoked by user.',
+        ToastAndroid.LONG,
+      );
+    }
+
+    return false;
+  };
+
+  /************************************ RENDERS ************************************/
+
   render() {
+    let {permissions} = this.state;
+
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
         <Text style={styles.text}>Current Location</Text>
+        <Text style={styles.text}>
+          Permissions: {permissions ? 'True' : 'False'}
+        </Text>
       </View>
     );
   }
